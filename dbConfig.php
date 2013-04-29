3ee9914ac406741534852b0710d27a9d8ba689bc
vimlesh <vimlesh.rajput@proptiger.com>
2013-04-29 17:08:25 +0530

    update file db name

--- dbConfig.php
+++ dbConfig.php
@@ -2,6 +2,8 @@
 
 $db = mysql_connect("192.168.1.136", "staging", "staging");
 //$dblink = mysql_select_db("proptiger", $db);
-$dblink = mysql_select_db("staging_project", $db);
+$dblink = mysql_select_db("project", $db);
+
+
 
 ?>
