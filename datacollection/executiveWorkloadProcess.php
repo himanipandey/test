10eb71d76a110bd7cb5dea107d5c3c5adec629e1
Azitabh Ajit <azitabh.ajit@proptiger.com>
2013-06-20 21:41:36 +0530

    remaining file in executive workload

--- datacollection/executiveWorkloadProcess.php
+++ datacollection/executiveWorkloadProcess.php
@@ -1,13 +1,6 @@
 <?php
-function getExecutiveWorkLoad(){
-    $sql = "select pa.ADMINID, pa.USERNAME, max(t.TOTAL) WORKLOAD from (select pa.ADMINID, 0 TOTAL from proptiger_admin pa union select pa.ADMINID, count(rp.MOVEMENT_HISTORY_ID) TOTAL from proptiger_admin pa inner join project_assignment pa1 on pa.ADMINID = pa1.ASSIGNED_TO inner join resi_project rp on pa1.MOVEMENT_HISTORY_ID = rp.MOVEMENT_HISTORY_ID  group by pa.ADMINID) t inner join proptiger_admin pa on t.ADMINID = pa.ADMINID group by pa.ADMINID order by WORKLOAD;";
-    return $result = dbQuery($sql);
-}
+require_once "$_SERVER[DOCUMENT_ROOT]/datacollection/functions.php";
 
-
-$executiveWorkLoad = getExecutiveWorkLoad();
-
+$executiveWorkLoad = getCallCenterExecutiveWorkLoad();
 $smarty->assign("executiveWorkLoad", $executiveWorkLoad);
-
-
 ?>
