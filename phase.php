42f6f7b5537fc27b5c642258a878565bcb5097e5
Azitabh Ajit <azitabh.ajit@proptiger.com>
2013-07-29 17:39:45 +0530

    reindentation

--- phase.php
+++ phase.php
@@ -1,13 +1,13 @@
 <?php
 
-	include("smartyConfig.php");
-	include("appWideConfig.php");
-	include("dbConfig.php");
-	include("includes/configs/configs.php");
-	include("builder_function.php");
-	AdminAuthentication();
-	include('phaseProcess.php');
-	$smarty->display(PROJECT_ADD_TEMPLATE_PATH."header.tpl");
-	$smarty->display(PROJECT_ADD_TEMPLATE_PATH."phase.tpl");
-	$smarty->display(PROJECT_ADD_TEMPLATE_PATH."footer.tpl");
+include("smartyConfig.php");
+include("appWideConfig.php");
+include("dbConfig.php");
+include("includes/configs/configs.php");
+include("builder_function.php");
+AdminAuthentication();
+include('phaseProcess.php');
+$smarty->display(PROJECT_ADD_TEMPLATE_PATH . "header.tpl");
+$smarty->display(PROJECT_ADD_TEMPLATE_PATH . "phase.tpl");
+$smarty->display(PROJECT_ADD_TEMPLATE_PATH . "footer.tpl");
 ?>
