8e170d6389543705c3bc685382bfa1b257a11c56
Sagar Amit <amit.sagar@proptiger.com>
2013-08-15 16:53:39 +0530

    change display order

--- duplicateBuilder.php
+++ duplicateBuilder.php
@@ -8,7 +8,7 @@
     $smarty->display(PROJECT_ADD_TEMPLATE_PATH."header.tpl");
 
     $data = array();
-    $query = "select BUILDER_ID, BUILDER_NAME, BUILDER_IMAGE, ENTITY from project.resi_builder where ACTIVE = '1' order by BUILDER_NAME asc";
+    $query = "select BUILDER_ID, BUILDER_NAME, BUILDER_IMAGE, ENTITY from project.resi_builder where ACTIVE = '1' order by BUILDER_ID asc";
     $res = mysql_query($query) or die(mysql_error());
     while($row = mysql_fetch_array($res)){
         array_push($data, $row); 
