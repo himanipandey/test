a520bde7c265244cfbb6e888d38b26b36ca7a290
Azitabh Ajit <azitabh.ajit@proptiger.com>
2013-09-10 17:00:01 +0530

    removing unused models

--- models/DisplayProjectOrder.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Model integration for bank list
-class DisplayProjectOrder extends ActiveRecord\Model
-{
-    static $table_name = 'display_project_order';
-}
--- models/InfrastructureCategory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Model integration for bank list
-class InfrastructureCategory extends ActiveRecord\Model
-{
-    static $table_name = 'infrastructure_category';
-}
