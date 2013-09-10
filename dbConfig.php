00a1cb7baeb4f61746442fa0c10caf64e64d762a
Hemendra <hemendra.srivastava@proptiger.com>
2013-09-10 14:47:33 +0530

    Revert "dbc"
    This reverts commit 0adc2bc09f5e0e08d7e3e07215f1d8028ac9b6e3.

--- dbConfig.php
+++ dbConfig.php
@@ -1,12 +1,4 @@
 <?php
-
-//  Site Database
-define("DB_PROJECT_HOST", "192.168.1.1");
-define("DB_PROJECT_USER", "root");
-define("DB_PROJECT_PASS", "root");
-define("DB_PROJECT_NAME", "project");
-
-
-$db = mysql_connect(DB_PROJECT_HOST, DB_PROJECT_USER, DB_PROJECT_PASS);
-$dblink = mysql_select_db(DB_PROJECT_NAME, $db);
+$db = mysql_connect("localhost", "root", "root");
+$dblink = mysql_select_db("project", $db);
 ?>
