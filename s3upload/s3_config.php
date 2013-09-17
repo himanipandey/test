b5de221100006e55a7fc74f5b0f6664e830670d7
Paritosh Singh <paritosh.singh@proptiger.com>
2013-09-17 12:42:07 +0530

    S3 bucket name changed to proptiger-img

--- s3upload/s3_config.php
+++ s3upload/s3_config.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__)."/s3/aws_file_manager.php";
 require_once dirname(__FILE__)."/upload.php";
 
 //define("S3_STORAGE_PATH","img/");
-$bucket="testing-proptiger";
+$bucket="proptiger-img";
 
 $s3 = AwsFileManager::get_s3_object('AKIAI5FTEFLES7UMOD4A', 'HMvOkDtE4OtZJFPkGozE7lEaFuKUWZbcjnNdWnSm');
 ?>
