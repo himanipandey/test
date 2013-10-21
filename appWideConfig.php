4cf8223e4ace4d2cbf09323a0bed89bf2a86452a
vimlesh <vimlesh.rajput@proptiger.com>
2013-10-21 17:17:14 +0530

    display error

--- appWideConfig.php
+++ appWideConfig.php
@@ -57,7 +57,7 @@ else{
 	define("JS_SERVER","http://$t" . $gblData['beyondServerDocRoot']);
 	define("CSS_SERVER","http://$t" . $gblData['beyondServerDocRoot']);
 	error_reporting(E_ALL);
-	ini_set('display_errors','0');
+	ini_set('display_errors','1');
 	ini_set('log_errors','1');
 
 	define("BEANSTALK_SERVER", 'localhost');
