f71bd64342e98332be59864e4eddc1e72d6f121a
vimlesh <vimlesh.rajput@proptiger.com>
2013-11-01 16:02:33 +0530

    local changes to stg

--- includes/configs/phaseStageConfig.php
+++ includes/configs/phaseStageConfig.php
@@ -1,10 +1,11 @@
 <?php
-    /***********stages*******/
+    /*************stages*******/
     define("NewProject_stage","NewProject");
     define("NoStage_stage","NoStage");
     define("SecondaryPriceCycle_stage","SecondaryPriceCycle");
     define("UpdationCycle_stage","UpdationCycle");
-    /***********phases*******/
+    
+    /**************phases*******/
     define("Audit1_phase","Audit1");
     define("Audit2_phase","Audit2");
     define("Complete_phase","Complete");
