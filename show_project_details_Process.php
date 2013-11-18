cd3a4b4346629bb5ed981c4a22802c53df5f20ee
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2013-11-18 17:39:05 +0530

    Efeective date format fixes

--- show_project_details_Process.php
+++ show_project_details_Process.php
@@ -41,9 +41,8 @@ foreach($optionsDetails as $key => $value) {
     $uptionDetailWithPrice[$value->phase_id][$value->option_id]['size'] = $value->size;
     $uptionDetailWithPrice[$value->phase_id][$value->option_id]['villa_plot_area'] = $value->villa_plot_area;
     $uptionDetailWithPrice[$value->phase_id][$value->option_id]['villa_no_floors'] = $value->villa_no_floors;
-    $uptionDetailWithPrice[$value->phase_id][$value->option_id]['effective_date'] = $listing_price[0]->effective_date;
-    
-    
+    $uptionDetailWithPrice[$value->phase_id][$value->option_id]['effective_date'] = date('Y-m-d',strtotime($listing_price[0]->effective_date));
+   
 }
 
 $PhaseOptionHash = $ProjectPhases[1];
