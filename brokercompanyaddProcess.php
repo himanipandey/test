2e3b9420c5d344cfda36c456dd2e2d6f98e3f29d
proptiger <proptiger@proptiger-desktop.(none)>
2013-12-06 17:31:46 +0530

    Image upload in progres

--- brokercompanyaddProcess.php
+++ brokercompanyaddProcess.php
@@ -238,9 +238,8 @@
                 
                 if(!is_dir("images_new"))
                 {
-                    echo $newImagePath."/images/images_new/<br>";
-                    mkdir("images_new" , 0777) or die("Can't");
-                    chmod($newImagePath."/images/images_new/" , 0777);
+                    mkdir("/home/sysadmin/public_html/images_new/" , 0777) or die("Can't");
+                    chmod("/home/sysadmin/public_html/images_new/" , 0777);
                     echo "he111e";
                     die;
                 }
