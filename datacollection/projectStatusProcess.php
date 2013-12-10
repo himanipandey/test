1cebea5da2ad7bdc1357a7e60d95aca13737e8bb
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2013-12-10 16:09:34 +0530

    144-debug

--- datacollection/projectStatusProcess.php
+++ datacollection/projectStatusProcess.php
@@ -85,7 +85,7 @@ function prepareDisplayData($data){
 	$sql = "select group_concat(pa1.DEPARTMENT order by pa.ID desc) as department from resi_project rp inner join project_stage_history psh on rp.project_id = psh.project_id inner join project_assignment pa on psh.HISTORY_ID = pa.MOVEMENT_HISTORY_ID inner join proptiger_admin pa1 on pa.ASSIGNED_TO = pa1.ADMINID where psh.history_id != rp.movement_history_id and rp.project_id in (".$pids.") group by rp.PROJECT_ID;";
 
 	$sql_dept = dbQuery($sql);
-        
+	        
     foreach ($data as $value) {
 		
 		  $prv_asg_dept = explode(",",$sql_dept[0]['department']);
@@ -102,8 +102,8 @@ function prepareDisplayData($data){
         $assignment_type = '';
         
         if($value['PREV_PROJECT_PHASE'] == 'Audit1' || $value['PREV_PROJECT_PHASE'] == 'Audit2'){
-            $assignment_type .= 'Reverted-';
-            if($prv_asg_dept[0] === 'SURVEY')$assignment_type .= 'Field ';
+            $assignment_type .= 'Reverted-'.$prv_asg_dept[0];
+            //if($prv_asg_dept[0] === 'SURVEY')$assignment_type .= 'Field ';
         }
         
         
