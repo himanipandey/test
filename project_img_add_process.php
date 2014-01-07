58a776e069d296cda9311123e140e4d6f7fd0e61
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2014-01-07 18:05:44 +0530

    IS-137 fixes

--- project_img_add_process.php
+++ project_img_add_process.php
@@ -89,7 +89,7 @@ if (isset($_POST['Next']))
 	$arrTitle = array();
 	$arrTaggedDate = array();
 	$arrTowerId = array();
-	//$arrDisplayOrder = array();
+	$arrDisplayOrder = array();
 	  	foreach($_FILES['txtlocationplan']['name'] as $k=>$v)
 		{
 			if($v != '')
@@ -114,7 +114,7 @@ if (isset($_POST['Next']))
 				$arrTitle[$k] = $_REQUEST['title'][$k];
 				$arrTaggedDate[$k] = $_REQUEST['txttagged_date'][$k+1];
 				$arrTowerId[$k] = $_REQUEST['txtTowerId'][$k+1]; 
-				//$arrDisplayOrder[$k] = $_REQUEST['txtdisplay_order'][$k+1];
+				$arrDisplayOrder[$k] = $_REQUEST['txtdisplay_order'][$k+1];
 			}
 		}
 				
@@ -993,6 +993,7 @@ if (isset($_POST['Next']))
 														TITLE	   = '".$arrTitle[$key]."',
                                                         SERVICE_IMAGE_ID   = ".$image_id.",
                                                         ".$add_tower." 
+                                                        DISPLAY_ORDER = '".$arrDisplayOrder[$key]."',
                                                         TAGGED_MONTH = '".$arrTaggedDate[$key]."'                                                       
 													WHERE PROJECT_ID = '".$projectId."'  AND PLAN_TYPE = '".$_REQUEST['PType']."' AND PLAN_IMAGE = '".$val."'";
 								$res	=	mysql_query($qry) or die(mysql_error());
@@ -1006,6 +1007,7 @@ if (isset($_POST['Next']))
 													    BUILDER_ID		=	'".$builderDetail['BUILDER_ID']."',
 													SERVICE_IMAGE_ID        =    ".$image_id.",
 													TITLE			=	'".$arrTitle[$key]."', 
+													DISPLAY_ORDER = '".$arrDisplayOrder[$key]."',
 													TAGGED_MONTH = '".$arrTaggedDate[$key]."',
 													".$add_tower."
 													SUBMITTED_DATE	=	now()";
