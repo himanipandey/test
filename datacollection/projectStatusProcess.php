8c8e8fd7e7be0820f209bee23a2a891a4fd3084c
vimlesh <vimlesh.rajput@proptiger.com>
2014-01-19 21:03:59 +0530

    version issue

--- datacollection/projectStatusProcess.php
+++ datacollection/projectStatusProcess.php
@@ -119,7 +119,7 @@ function prepareDisplayData($data){
                     on rp.project_id = psh.project_id inner join project_assignment pa on 
                     psh.HISTORY_ID = pa.MOVEMENT_HISTORY_ID inner join proptiger_admin pa1 on 
                     pa.ASSIGNED_TO = pa1.ADMINID where psh.history_id != rp.movement_history_id and 
-                    rp.project_id in (".$pids.") group by rp.PROJECT_ID;";
+                    rp.project_id in (".$pids.") and rp.version = 'Cms' group by rp.PROJECT_ID;";
 		$sql_depts = dbQuery($sql);
 	
 		foreach($sql_depts as $value){
@@ -154,7 +154,7 @@ function prepareDisplayData($data){
             $qryOldHistoryId = "select pa1.role from resi_project rp
                 join project_assignment pa on rp.movement_history_id = pa.movement_history_id
                 join  proptiger_admin pa1 on pa.assigned_to = pa1.adminid
-                where rp.project_id = ".$value['PROJECT_ID']." order by pa.updation_time asc limit 1";            
+                where rp.project_id = ".$value['PROJECT_ID']." and rp.version = 'Cms' order by pa.updation_time asc limit 1";            
             $resOldHistoryId = mysql_query($qryOldHistoryId) or die(mysql_error());
             $dataOldHistoryId = mysql_fetch_assoc($resOldHistoryId);
            // $firstRole = $dataOldHistoryId['role'];
