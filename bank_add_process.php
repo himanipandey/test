0aada3953ff83c0502ab8c2ea18d292ec50a7aa6
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2014-01-23 17:31:23 +0530

    IS-253 fixes

--- bank_add_process.php
+++ bank_add_process.php
@@ -8,7 +8,21 @@
 		$bank_detail=	trim($_REQUEST['bank_detail']);
 		$logo_name	=	$_FILES['logo']['name'];
 		
-		if($logo_name != ''){
+		
+		 if($bankid == '')   
+			$banks = new BankList();
+		 else
+			$banks = BankList::find($bankid);
+		
+		 $banks->bank_name = $bankname;
+		 $banks->bank_detail = $bank_detail;
+		 
+		$banks->status = "Active"; 
+        $banks->save();
+        
+        $bankid = $banks->bank_id;
+        
+        if($logo_name != '' && $bankid != ''){
 			$dest		=	$newImagePath."/bank_list/".$logo_name;
 			$move		=	move_uploaded_file($_FILES['logo']['tmp_name'],$dest);
 			if($move)
@@ -23,23 +37,17 @@
 				$image_id = $image_id->id;
 			}
 		 }
-		 if($bankid == '')   
-			$banks = new BankList();
-		 else
-			$banks = BankList::find($bankid);
-		
-		 $banks->bank_name = $bankname;
-		 $banks->bank_detail = $bank_detail;
-		 
-		 if($logo_name !='' && $image_id){
+        
+        if($logo_name !='' && $image_id){
 			$banks->bank_logo = $logo_name;
             $banks->service_image_id = $image_id;
          }elseif(isset($_POST['bankLogo']) && $_POST['bankLogo'] == 'del-logo'){
 			 $banks->bank_logo = '';
             $banks->service_image_id = 0;
 		 }
-         $banks->status = "Active"; 
+        
         $banks->save();
+        
 		header("Location:bank_list.php?page=1&sort=all");
 		
 	} 
