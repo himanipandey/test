a3a5fb4dc3ae07ea7e60046218b4f4cbb3995e1e
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2014-01-30 18:59:18 +0530

    IS-251- loc-avg fixes

--- builder_function.php
+++ builder_function.php
@@ -2205,7 +2205,7 @@ function getLocalityAveragePrice($locId)
 	
 	$locId = $locId - 50000;
 
-	$prev_month = date("Y-m", strtotime("-1 months"));
+	//$prev_month = date("Y-m", strtotime("-1 months"));
 
 	$keytoken = hash_hmac ( 'sha1' , $tmstmp , $psswd );
 	$url = $_SERVER['HTTP_HOST']."/app/v1/locality-price-trend?username=".$usrn."&token=".$keytoken."&timestamp=".$tmstmp."&locality=".$locId."&duration=3&unittype[]=apartment&unittype[]=plot&unittype[]=villa&duration=1";
@@ -2213,16 +2213,25 @@ function getLocalityAveragePrice($locId)
 	$obj=curlFetch($url);
 	$json=json_decode($obj,true);
 	
+		
 	$average_price = 0;
 	$total_price = 0;
 	$cnt = 0;
 	foreach($json['price_trend'] as $unit_type => $price_array){
-			$total_price += $price_array[$prev_month]['mean'];
-			$cnt++;
+		$count=0;
+		foreach($price_array as $key=>$value){
+			if($count == 0){
+				$total_price += $value['mean'];
+				$cnt++;
+			}
+			else
+				continue;
+		}
+		
+			
 	}
 	
 	$average_price = round($total_price / $cnt);
-	
 	return $average_price ;
 }
 ?>
