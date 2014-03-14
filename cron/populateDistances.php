6b98d09cb82f7cc056573b31363db8c684f83262
Azitabh Ajit <azitabh@gmail.com>
2014-03-14 13:37:00 +0530

    adding functions

--- cron/populateDistances.php
+++ cron/populateDistances.php
@@ -29,11 +29,8 @@ $aMaxLandmarkCount = array(1=>100, 2=>50);
 $connection = ActiveRecord\Connection::instance();
 
 truncateTable('landmark_distances');
-
-
-$sql = "insert into landmark_distances (object_id, object_type, landmark_id, distance, city_id, place_type_id, priority) select rp.PROJECT_ID, 'Project', ld.id, ((ACOS(SIN(rp.LATITUDE * PI() / 180) * SIN(ld.latitude * PI() / 180) + COS(rp.LATITUDE * PI() / 180) * COS(ld.latitude * PI() / 180) * COS((rp.LONGITUDE - ld.longitude) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1609.34) AS distance, ld.city_id, ld.place_type_id, ld.priority from resi_project rp inner join locality l on rp.LOCALITY_ID = l.LOCALITY_ID inner join suburb s on l.SUBURB_ID = s.SUBURB_ID inner join landmarks ld on s.CITY_ID = ld.city_id where rp.version = 'Website' and rp.LONGITUDE not in (" . implode(",", $aInvalidCoordinates) . ") and rp.LATITUDE not in (" . implode(",", $aInvalidCoordinates) . ") and ld.status = 'Active' and ld.place_type_id in (" . implode(",", $aAllLandmarkTypes) . ")";
-
-$connection->query($sql);
+insertDistances();
+deleteInsignificantEntries();
 
 
 foreach($aMaxLandmarkDistance as $k=>$v){
@@ -41,11 +38,37 @@ foreach($aMaxLandmarkDistance as $k=>$v){
   $connection->query($sql);
 }
 
-foreach($aMaxLandmarkCount as $landmarkType=> $count) {
-    $sql = "select object_id, object_type from landmark_distances where place_type_id = $landmarkType group by object_id, object_type";
-    $aRow = ResiProject::find_by_sql($sql);
-    foreach ($aRow as $row) {
-        $sql = "delete ld.* from landmark_distances ld inner join (select id, distance from landmark_distances where object_id = $row->object_id and object_type = '$row->object_type' and place_type_id = $landmarkType order by distance limit 100000 OFFSET $count) t on ld.id = t.id;";
+function insertDistances(){
+    global $connection, $aInvalidCoordinates, $aAllLandmarkTypes;
+
+    $sql = "insert into landmark_distances (object_id, object_type, landmark_id, distance, city_id, place_type_id, priority) select rp.PROJECT_ID, 'Project', ld.id, ((ACOS(SIN(rp.LATITUDE * PI() / 180) * SIN(ld.latitude * PI() / 180) + COS(rp.LATITUDE * PI() / 180) * COS(ld.latitude * PI() / 180) * COS((rp.LONGITUDE - ld.longitude) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1609.34) AS distance, ld.city_id, ld.place_type_id, ld.priority from resi_project rp inner join locality l on rp.LOCALITY_ID = l.LOCALITY_ID inner join suburb s on l.SUBURB_ID = s.SUBURB_ID inner join landmarks ld on s.CITY_ID = ld.city_id where rp.version = 'Website' and rp.LONGITUDE not in (" . implode(",", $aInvalidCoordinates) . ") and rp.LATITUDE not in (" . implode(",", $aInvalidCoordinates) . ") and ld.status = 'Active' and ld.place_type_id in (" . implode(",", $aAllLandmarkTypes) . ")";
+
+    $connection->query($sql);
+}
+
+function deleteInsignificantEntries(){
+    deleteEntriesBasedOnDistance();
+    deleteEntriesBasedOnCount();
+}
+
+function deleteEntriesBasedOnDistance(){
+    global $connection, $aMaxLandmarkDistance;
+
+    foreach($aMaxLandmarkDistance as $k=>$v){
+        $sql = "delete from landmark_distances where place_type_id = $k and distance > $v";
         $connection->query($sql);
+    }
+}
+
+function deleteEntriesBasedOnCount(){
+    global $connection, $aMaxLandmarkCount;
+    
+    foreach($aMaxLandmarkCount as $landmarkType=> $count) {
+        $sql = "select object_id, object_type from landmark_distances where place_type_id = $landmarkType group by object_id, object_type";
+        $aRow = ResiProject::find_by_sql($sql);
+        foreach ($aRow as $row) {
+            $sql = "delete ld.* from landmark_distances ld inner join (select id, distance from landmark_distances where object_id = $row->object_id and object_type = '$row->object_type' and place_type_id = $landmarkType order by distance limit 100000 OFFSET $count) t on ld.id = t.id;";
+            $connection->query($sql);
+        }
     }
 }
