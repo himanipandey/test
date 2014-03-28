cffbb3171c0b4e121ec9f66ea068b9f45f1ae127
vimlesh <vimlesh.rajput@proptiger.com>
2014-03-28 18:18:02 +0530

    create new branch

--- cron/cronConfig.php
+++ cron/cronConfig.php
@@ -6,7 +6,8 @@ require_once 'cronFunctions.php';
 require_once $docroot.'includes/send_mail_amazon.php';
 
 $latLongList = '0,1,2,3,4,5,6,7,8,9';
-$dailyEmail = array(
+$currentDate = date("Y-m-d");
+/*$dailyEmail = array(
 	array(
 		'sql'=>"SELECT 
                             rp.PROJECT_ID, rp.PROJECT_NAME, rb.BUILDER_NAME, rp.PROJECT_URL, c.LABEL as CITY 
@@ -54,7 +55,7 @@ $dailyEmail = array(
             inner join resi_builder rb on rp.builder_id = rb.builder_id
              where 
             l.IS_GEO_BOUNDARY_CLEAN = 'true'
-            and rp.version = 'Cms'
+            and rp.version = 'Cms' and rp.status in('Active','ActiveInCms')
             and ((rp.LONGITUDE not between l.MIN_LONGITUDE and l.MAX_LONGITUDE) or (rp.LATITUDE not between l.MIN_LATITUDE and l.MAX_LATITUDE))
              and (rp.LATITUDE not in($latLongList) or rp.LONGITUDE not in($latLongList));",
             'subject'=>'Lat Long Beyond Limits',
@@ -62,7 +63,28 @@ $dailyEmail = array(
             'attachmentname'=>'Latitude_longitude_beyond_limit',
             'sendifnodata'=>0
         )
-);
+);*/
 
- 
+$dailyEmail = array(
+     array(
+            'sql'=>"select rp.PROJECT_ID, rp.PROJECT_NAME from resi_project rp 
+             where 
+                rp.version = 'Cms'
+                and rp.status in('Active','ActiveInCms') and project_status_id = 2;",
+                'subject'=>'Cancelled projects but not yet marked inactive',
+                'recipients'=>array('vimlesh.rajput@proptiger.com','manish.goyal@proptiger.com','mohit.dargan@proptiger.com'), 
+                'attachmentname'=>'Cancelled_projecst_but_not_yet_marked_inactive',
+                'sendifnodata'=>0
+        ),
+     array(
+            'sql'=>"select rp.PROJECT_ID, rp.PROJECT_NAME from resi_project rp 
+             where 
+                rp.version = 'Cms' and rp.status in('Active','ActiveInCms') and project_status_id in(8,1,7) and promised_completion_date < '$currentDate'
+                    and promised_completion_date != '0000-00-00';",
+               'subject'=>'Projects whose status is Pre Launch,Under construction,Launch projects but Expected completion date is in past',
+               'recipients'=>array('vimlesh.rajput@proptiger.com','manish.goyal@proptiger.com','mohit.dargan@proptiger.com'), 
+               'attachmentname'=>'projects_whose_status_is_pre_launch_under_construction_launch_projects_but_expected_completion_date_is_in_past',
+               'sendifnodata'=>0
+        )   
+);
 ?>
