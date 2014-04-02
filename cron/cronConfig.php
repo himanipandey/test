e2a9afdbbed50b7b0d3b891233db0173557adfef
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2014-04-02 19:36:54 +0530

    IS-326 fixes

--- cron/cronConfig.php
+++ cron/cronConfig.php
@@ -74,7 +74,7 @@ $dailyEmail = array(
         array(
             'sql'=>"SELECT id as OFFER_ID,project_id as PROJECT_ID,OFFER,OFFER_DESC,created_at as START_DATE,OFFER_END_DATE,STATUS FROM `project_offers` WHERE STATUS = 'Active' AND OFFER_END_DATE='".$future_date."';",
             'subject'=>'Project Offers Reaching Expiry Date',
-            'recipients'=>array('kuldeep.patel_c@proptiger.com'), 
+            'recipients'=>array('ankur.dhawan@proptiger.com'), 
             'attachmentname'=>'expired_project_offers',
             'message'=>"Please extend the offers validity otherwise they will be deactivated.",
             'sendifnodata'=>0
