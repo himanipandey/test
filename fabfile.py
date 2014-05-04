4bd1dd9aed83d3c9f38999125693df89f264f176
mmpandeyiitd <mmpandeyiitd@gmail.com>
2014-05-05 04:04:52 +0530

    change in fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ def deploy_tag(tag):
   put('/tmp/cms.tar.gz', '/tmp')
   run("cd /home/sysadmin && mkdir cms_new && cd cms_new && tar -xf /tmp/cms.tar.gz && ln -s /home/sysadmin/public_html/images_new images_new && cp -r /home/sysadmin/cms_config_prod/* . && cd /home/sysadmin && mv production_cms cms_old && mv cms_new production_cms && echo %s > production_cms/version && sudo rm -rf /tmp/*.tpl.php && rm -r cms_old" %(tag))
 
-#fab -Hsysadmin@cms.proptiger-ws.com deploy_branch:branchname  this is the command for the code to be deployed on staging server
+#fab -Hsysadmin@cms.proptiger-ws.com deploy_stg_branch:branchname  this is the command for the code to be deployed on staging server
 def deploy_stg_branch(branch):
   local("git archive --format tar.gz --output /tmp/cms.tar.gz remotes/origin/%s" %(branch))
   put('/tmp/cms.tar.gz', '/tmp')
