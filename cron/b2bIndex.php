958149da5aaef6fbcc18247ef0e21fd5582d7bfe
Azitabh Ajit <azitabh@gmail.com>
2014-06-15 17:15:34 +0530

    change in table population script

--- cron/b2bIndex.php
+++ cron/b2bIndex.php
@@ -96,7 +96,7 @@ DInventoryPriceTmp::setPeriodLaunchedUnits();
 DInventoryPriceTmp::setInventoryOverhang();
 DInventoryPriceTmp::populateDemand();
 
-if (true || runTests()) {
+if (runTests()) {
     DInventoryPriceTmp::connection()->query("rename table d_inventory_prices to d_inventory_prices_old, d_inventory_prices_tmp to d_inventory_prices, d_inventory_prices_old to d_inventory_prices_tmp;");
     $logger->info("Migration successful.");
 } else {
