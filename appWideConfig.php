9e2151a7a9d46b110e663169324aa4ccc31dbdd4
vimlesh <vimlesh.rajput@proptiger.com>
2014-06-26 18:26:05 +0530

    add ground floor plan

--- appWideConfig.php
+++ appWideConfig.php
@@ -81,7 +81,7 @@ else{
     define("CLOUDAGENT_USER", "proptiger");
     define("CLOUDAGENT_KEY", "KK6553cb21f45e304ffb6c8c92a279fde5");
 
-	define("IMAGE_SERVICE_URL","http://dev-api.proptiger-ws.com:8080/data/v1/entity/image");
+	define("IMAGE_SERVICE_URL","http://qa.proptiger-ws.com:8080/data/v1/entity/image");
     define("SERVER_PATH_SOLR_RESTART", "/home/sysadmin/nightlytest.proptiger.com");  //for staging
     //define("SERVER_PATH_SOLR_RESTART", "/home/sysadmin/production/");  //for server
 }
