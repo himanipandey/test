6b316b30236e658a5c947d1c700baa2f6cd1a5cc
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2014-07-07 18:25:46 +0530

    IS-293-V2 fxies

--- models/ProjectSupply.php
+++ models/ProjectSupply.php
@@ -223,12 +223,9 @@ class ProjectSupply extends Objects {
         $supply = intval($supply);
         if ($phaseId == '0')
             $phaseId = NULL;
-       
-        $supply_new = mysql_query("SELECT `project_supplies`.* FROM `project_supplies` join listings l on (l.id = project_supplies.listing_id and l.phase_id = '$phaseId') join resi_project_options o on (l.option_id = o.options_id and " . ($noOfBedroom == null ? "(o.bedrooms is null OR o.bedrooms = 0)" : "o.bedrooms = $noOfBedroom") . " and o.option_type='$projectType') WHERE `project_supplies`.`version`='PreCms'");
-       
+        $supply_new = mysql_fetch_object(mysql_query("SELECT `project_supplies`.* FROM `project_supplies` join listings l on (l.id = project_supplies.listing_id and l.phase_id = '$phaseId') join resi_project_options o on (l.option_id = o.options_id and " . ($noOfBedroom == null ? "(o.bedrooms is null OR o.bedrooms = 0)" : "o.bedrooms = $noOfBedroom") . " and o.option_type='$projectType') WHERE `project_supplies`.`version`='PreCms'"));
         if ($supply_new) {
-            $supplyId = $supply_new[0]->id;
-            $supplyId = $supply_new[0]->id;
+            $supplyId = $supply_new->id;
             $availability = ProjectAvailability::getAvailability($supplyId);
             if ($availability > $launchedUnit && $availability > 0 && $supply >= 0 && $launchedUnit >= 0) {
                 return false;
