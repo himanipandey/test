7af67abdcfba29422fdb66fce95296397fd0b811
pawan kumar <pawan.kumar1@proptiger.com>
2014-07-21 13:18:21 +0530

    Period attributes like quarter,year added for construction_status

--- cron/b2bIndex.php
+++ cron/b2bIndex.php
@@ -238,7 +238,11 @@ function setConfigLevelValues(&$entry) {
     $entry['builder_name'] = $configDetails->builder_name;
     $entry['builder_headquarter_city'] = $configDetails->builder_headquarter_city;
     $entry['construction_status'] = computeConstructionStatus($configDetails,$entry['effective_month']);
-    $entry['unit_type'] = $configDetails->unit_type;
+    $entry['construction_status_quarter'] = computeConstructionStatus($configDetails,date_format(lastDayOf('quarter', $entry['effective_month']),'Y-m-d'));
+    $entry['construction_status_year'] = computeConstructionStatus($configDetails,date_format(lastDayOf('year', $entry['effective_month']),'Y-m-d'));
+    $entry['construction_status_financial_year'] = computeConstructionStatus($configDetails,date_format(lastDayOf('financial_year', $entry['effective_month']),'Y-m-d'));
+    
+   
     $entry['bedrooms'] = intval($configDetails->bedrooms);
     $entry['average_size'] = $configDetails->average_size;
     $entry['completion_date'] = $configDetails->completion_date;
@@ -256,15 +260,24 @@ function computeConstructionStatus($configDetails, $effectiveMonth) {
 	$completionDate   = $configDetails->completion_date;
 	$launchDate       = $configDetails->launch_date;
 	
+	echo "\n completion date==".$completionDate."  launch date ==".$launchDate."   effective month==".$effectiveMonth ;
+	
+	
 	$status = 'Pre Launch';
 
-	if (!empty($launchDate) && $effectiveMonth >= $launchDate) {
+
+	if (!empty($launchDate) && strcmp($effectiveMonth,$launchDate)>=0) {
+	
+		
 		$status = 'Under Construction';
+		
 	}
 
-	if (!empty($completionDate) && $effectiveMonth >= $completionDate) {
+	if (!empty($completionDate) && strcmp($effectiveMonth,$completionDate)>=0) {
+		
 		$status = 'Completed';
-	}
+		
 	
+	}
 	return $status;
 }
