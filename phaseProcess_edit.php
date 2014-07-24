afab177eb87d66ac25d65d5780432bd5729712a7
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2014-07-24 19:04:30 +0530

    IS-454 fxies

--- phaseProcess_edit.php
+++ phaseProcess_edit.php
@@ -446,27 +446,17 @@ if (isset($_POST['btnSave'])) {
 		   }
 		  
            if($completion_date != ''){			   	
-			   if( $yearExp2[0] == date("Y") ) {				   
-				   if( intval($yearExp2[1]) < intval(date("m"))) {
-					 $error_msg = "Completion date cannot be less than the current month in case of Construction Status is $construction_status_text.";
-				   }    
-			   } 
-			   else if (intval($yearExp2[0]) < intval(date("Y")) ) {
-				  $error_msg = "Completion date cannot be less than the current month in case of Construction Status is $construction_status_text.";
-			   }
+			   if(strtotime($completion_date) < time()) {				   				 
+				 $error_msg = "Completion date cannot be past date in case of Construction Status is $construction_status_text.";		
+			   } 			  
 		   }else{
 			   $error_msg = "Completion date is required in case of Construction Status is $construction_status_text."; 
 		   }	
         }elseif($construction_status == OCCUPIED_ID_3 || $construction_status == READY_FOR_POSSESSION_ID_4 ){
 			$yearExp = explode("-",$completion_date);
-            if( $yearExp[0] == date("Y") ) {
-                if( intval($yearExp[1]) > intval(date("m"))) {
-                  $error_msg = "Completion date cannot be greater current month in case of Construction Status is Completed.";
-                }    
+            if(strtotime($completion_date) > time()) {
+               $error_msg = "Completion date cannot be future date in case of Construction Status is Completed.";                
             } 
-            else if (intval($yearExp[0]) > intval(date("Y")) ) {
-                $error_msg = "Completion date cannot be greater current month in case of Construction Status is Completed.";
-            }
             if($launch_date == ''){
 				$error_msg = "Launch date is required in case of Construction Status is Completed.";
 			}
