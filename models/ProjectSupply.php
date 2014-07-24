81951ed3999c1cef52812a1ac939f9540c986504
vimlesh <vimlesh.rajput@proptiger.com>
2014-07-24 20:47:34 +0530

    is-440

--- models/ProjectSupply.php
+++ models/ProjectSupply.php
@@ -86,9 +86,10 @@ class ProjectSupply extends Objects {
 
     function projectSupplyForProjectPage($projectId) {
         $result = array();
-        $query = "select rpp.PHASE_NAME, rpp.LAUNCH_DATE, rpp.COMPLETION_DATE, rpp.submitted_date, rpp.project_id, rpp.BOOKING_STATUS_ID,
-            psm.display_name as construction_status, ls.phase_id, rpo.bedrooms as no_of_bedroom, ps.supply, ps.launched, 
+        $query = "select rpp.PHASE_NAME, rpp.LAUNCH_DATE, rpp.COMPLETION_DATE, rpp.submitted_date, rpp.project_id,  rpp.BOOKING_STATUS_ID,
+            ls.phase_id, rpo.bedrooms as no_of_bedroom, ps.supply, ps.launched, 
             pa.availability, pa.comment, pa.effective_month, rpo.option_type as project_type,ls.id as listing_id
+             ,psm.display_name as construction_status
             from 
              " . self::table_name() . " ps 
              inner join " . ProjectAvailability::table_name() . " pa on (ps.id=pa.project_supply_id and ps.version = 'PreCms')
@@ -99,19 +100,17 @@ class ProjectSupply extends Objects {
                     from " . self::table_name() . " ps inner join " . ProjectAvailability::table_name() . " pa 
                     on ps.id=pa.project_supply_id
                     inner join listings ls on (ps.listing_id = ls.id and ls.listing_category = 'Primary' and ls.status = 'Active')  
-                    left join " . ResiProjectPhase::table_name() . " rpp on ls.phase_id = rpp.PHASE_ID
-                    join project_status_master psm on rpp.construction_status = psm.id    
-                    where rpp.project_id = $projectId and rpp.version = 'Cms' and ps.version = 'Cms' and ps.version = 'PreCms' and rpp.status = 'Active' group by ps.id
-
+                    left join " . ResiProjectPhase::table_name() . " rpp on ls.phase_id = rpp.PHASE_ID 
+                    where rpp.project_id = $projectId and rpp.version = 'Cms' and ps.version = 'PreCms' and rpp.status = 'Active' group by ps.id
                  ) t 
                 on ps.id=t.id and pa.effective_month=t.mon 
              left join " . ResiProjectPhase::table_name() . "  rpp on (ls.phase_id = rpp.PHASE_ID and rpp.version = 'Cms')
-             join project_status_master psm on rpp.construction_status = psm.id    
-       union 
+             join project_status_master psm on rpp.construction_status = psm.id  
+      union 
             select rpp.PHASE_NAME, rpp.LAUNCH_DATE, 
-                rpp.COMPLETION_DATE, rpp.submitted_date, rpp.project_id,rpp.BOOKING_STATUS_ID, psm.display_name as construction_status, ls.phase_id, rpo.bedrooms as no_of_bedroom, ps.supply,
+                rpp.COMPLETION_DATE, rpp.submitted_date, rpp.project_id,rpp.BOOKING_STATUS_ID, ls.phase_id, rpo.bedrooms as no_of_bedroom, ps.supply,
                 ps.launched, pa.availability, pa.comment, pa.effective_month, 
-                rpo.option_type as project_type,ls.id as listing_id 
+                rpo.option_type as project_type,ls.id as listing_id , psm.display_name as construction_status
             from 
                 project_supplies ps left join project_availabilities pa on (ps.id=pa.project_supply_id and ps.version = 'PreCms')
             inner join listings ls on (ps.listing_id = ls.id  and ls.listing_category = 'Primary' and ls.status = 'Active')          
@@ -136,7 +135,6 @@ class ProjectSupply extends Objects {
             $entry['SUBMITTED_DATE'] = $value->effective_month;
             $entry['PROJECT_TYPE'] = $value->project_type;
             $entry['BOOKING_STATUS_ID'] = $value->booking_status_id;
-            $entry['CONSTRUCTION_STATUS'] = $value->construction_status;
             $entry['LISTING_ID'] = $value->listing_id;
             $result[] = $entry;
         }
