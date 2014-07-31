f5e198781bc1aa7b9827a1ca7edca259f69d5c05
kuldeep.patel <kuldeep.patel_c@proptiger.com>
2014-07-31 13:38:54 +0530

    Kprint-16 fixes

--- phaseProcess_edit.php
+++ phaseProcess_edit.php
@@ -452,24 +452,35 @@ if (isset($_POST['btnSave'])) {
 			   $error_msg = "Launch date is required in case of Construction Status is $construction_status_text."; 
 		   }
 		  
-           if($completion_date != ''){			   	
-			   if(strtotime($completion_date) < time()) {				   				 
-				 $error_msg = "Completion date cannot be past date in case of Construction Status is $construction_status_text.";		
-			   } 			  
+           if($completion_date != ''){	
+			   if( $yearExp2[0] == date("Y") ) {
+				 if( intval($yearExp2[1]) < intval(date("m"))) {
+					$error_msg = "Completion date cannot be past date in case of Construction Status is $construction_status_text.";	
+				  }    
+				} 
+				else if (intval($yearExp2[0]) < intval(date("Y")) ) {
+				  $error_msg = "Completion date cannot be past date in case of Construction Status is $construction_status_text.";
+				}		   				  			  
 		   }else{
 			   $error_msg = "Completion date is required in case of Construction Status is $construction_status_text."; 
 		   }	
         }elseif($construction_status == OCCUPIED_ID_3 || $construction_status == READY_FOR_POSSESSION_ID_4 ){
 			$yearExp = explode("-",$completion_date);
-            if(strtotime($completion_date) > time()) {
-               $error_msg = "Completion date cannot be future date in case of Construction Status is Completed.";                
-            } 
+            if($completion_date != ''){	
+			   if( $yearExp2[0] == date("Y") ) {
+				 if( intval($yearExp2[1]) > intval(date("m"))) {
+					 $error_msg = "Completion date cannot be future date in case of Construction Status is Completed.";	
+				  }    
+				} 
+				else if (intval($yearExp2[0]) > intval(date("Y")) ) {
+				   $error_msg = "Completion date cannot be future date in case of Construction Status is Completed.";
+				}		   				  			  
+		    }else{
+			  $error_msg = "Completion date is required in case of Construction Status is Completed.";
+		    }
             if($launch_date == ''){
 				$error_msg = "Launch date is required in case of Construction Status is Completed.";
-			}
-			if($completion_date == ''){
-				$error_msg = "Completion date is required in case of Construction Status is Completed.";
-			}			
+			}					
 		}elseif( $construction_status == PRE_LAUNCHED_ID_8 && $phase_pre_launch_date == '') { 
            $error_msg = "Phase Status can not be Pre Launched in case of Pre Launched Date is blank.";
         }elseif( $construction_status == PRE_LAUNCHED_ID_8 && $launch_date != '') { 
