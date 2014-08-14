42506fab7705d817485e905dafb0bde580572f6c
Ahsan Khan <ahsan.khan@proptiger.com>
2014-08-14 13:49:58 +0530

    [#MIDL-402] Locality score enhancements changes

--- models/ProjectLivability.php
+++ models/ProjectLivability.php
@@ -156,7 +156,7 @@ class ProjectLivability extends ActiveRecord\Model {
     }
     
     static function populateLivabilityInProjects(){
-    	$sql = "update resi_project rp inner join project_livability pl on rp.project_id = pl.project_id set rp.livability_score = ROUND(pl.livability*10,1)";
+    	$sql = "update resi_project rp inner join project_livability pl on rp.project_id = pl.project_id set rp.livability_score = ROUND(pl.livability*10,1), rp.project_locality_score = ROUND(pl.locality_score*10,1), rp.project_society_score = ROUND(pl.society_score*10,1)";
     	self::connection()->query($sql);
     }
 }
