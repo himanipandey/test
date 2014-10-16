a4e271fd7f9fd431e1bc40716eb639a44b68af22
mmpandeyiitd <mmpandeyiitd@gmail.com>
2014-10-16 15:03:37 +0530

    save

--- couponGenerateProcess.php
+++ couponGenerateProcess.php
@@ -19,13 +19,14 @@ $res = mysql_query($query) or die(mysql_error());
 while($data = mysql_fetch_assoc($res)){
 	$data['coupon_price'] = moneyFormatIndia($data['coupon_price']);
 	if($data['discount_type']=='SqFt'){
+		
 		$data['discount'] = moneyFormatIndia($data['discount']/$data['SIZE']);
 	}
 	else
 	$data['discount'] = moneyFormatIndia($data['discount']);
 	array_push($catalogue, $data);
 }
-
+//echo moneyFormatIndia(23301);
 $smarty->assign("catalogue", $catalogue);
 
 $smarty->assign('url', TYPEAHEAD_API_URL);
@@ -35,7 +36,10 @@ function moneyFormatIndia($n){
 	
 	list($n,$dec)	=	 explode('.',$n);
 	$len			=	 strlen($n); //lenght of the no
-	$num			=	 substr($n,$len-3,3); //get the last 3 digits
+	if($len<3)
+		$num			=	 substr($n,0,3); //get the last 3 digits
+	else
+		$num			=	 substr($n,$len-3,3); //get the last 3 digits
 	$n				=	 floor($n/1000); //omit the last 3 digits already stored in $num
 	while($n > 0) //loop the process - further get digits 2 by 2
 	{
