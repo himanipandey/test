f1885799292caeacbd86769cb363c8892320d554
vimlesh <vimlesh.rajput@proptiger.com>
2014-12-04 20:48:02 +0530

    amenity

--- add_specificationProcess.php
+++ add_specificationProcess.php
@@ -68,7 +68,7 @@
                                 {  
                                         $amntChk = amenityCheck($projectId, $amenity_id);
                                         if($amntChk == true ){
-                                            $qryUpdateAmnt[] = "update resi_project_amenities set amenity_display_name = '".str_replace("_"," ",addslashes($_REQUEST[$key_display][0]))."' where project_id = $projectId and amenity_id = $amenity_id";
+                                            $qryUpdateAmnt[] = "update resi_project_amenities set amenity_display_name = '".str_replace("_"," ",addslashes($_REQUEST[$key_display][0]))."' where project_id = $projectId and amenity_id = $amenity_id and verified = 1";
                                         }else{
                                             //$isVerrified = amenityCheckVerified($projectId, $amenity_id);
                                             //if($isVerrified == true)
@@ -79,7 +79,7 @@
                                 {
                                     $amntChk = amenityCheck($projectId, $amenity_id);
                                     if($amntChk == true){ 
-                                        $qryUpdateAmnt[] = "update resi_project_amenities set amenity_display_name = '".str_replace("_"," ",addslashes($amenity_name))."' where project_id = $projectId and amenity_id = $amenity_id";
+                                        $qryUpdateAmnt[] = "update resi_project_amenities set amenity_display_name = '".str_replace("_"," ",addslashes($amenity_name))."' where project_id = $projectId and amenity_id = $amenity_id and verified = 1";
 
                                      }else  {   
                                         // $isVerrified = amenityCheckVerified($projectId, $amenity_id);
