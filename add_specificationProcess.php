d19fa2eba2a14336b607d4aff63953e97a2950d6
vimlesh <vimlesh.rajput@proptiger.com>
2014-12-17 19:56:04 +0530

    chk

--- add_specificationProcess.php
+++ add_specificationProcess.php
@@ -187,10 +187,10 @@
                         }
                     }else
                     {
-                        $ErrMsg1 = '3';
+                        $ErrMsg1 = 3;
                     }
-echo $ErrMsg1;die("here");
-                    if($ErrMsg1 == '3'){
+
+                    if($ErrMsg1 == 3){
                          $ErrMsg = 'Please delete existing listing amenities';
                          $smarty->assign("ErrMsg", $ErrMsg);
                     }
