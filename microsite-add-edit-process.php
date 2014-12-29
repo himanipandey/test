75ec27ae979536f91a2a274bd74ccf4c848238b5
vimleshrajput <vimlesh.rajput@proptiger.com>
2014-12-29 19:39:42 +0530

    validation

--- microsite-add-edit-process.php
+++ microsite-add-edit-process.php
@@ -137,7 +137,7 @@ if(isset($_REQUEST['searchProject'])){
         if(stristr(strtolower($_REQUEST['imageTitle'][$k]),'proptiger') || stristr(strtolower($_REQUEST['imageAlt'][$k]),'proptiger')
 	   || stristr(strtolower($_REQUEST['imageName'][$k]),'proptiger'))
            $ErrorMsg['imgTitleName'] = "Proptiger word is not allowed."; 
-	elseif($arrImage['imageTitle'][$k] == '' || $arrImage['imageAlt'][$k] == ''){
+	elseif(count($arrImage['imageTitle'][$k]) == 0 || count($arrImage['imageAlt'][$k]) == 0){
 	   $ErrorMsg['imgTitleName'] = "Slider Image name, Alt tag and title is mandatory.";
 	}
         //$ErrorMsg['configName'] = "Proptiger word is not allowed.";
