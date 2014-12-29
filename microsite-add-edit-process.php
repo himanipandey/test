82c060c71482454a63900acbf82d559774dc8b18
vimleshrajput <vimlesh.rajput@proptiger.com>
2014-12-29 19:54:32 +0530

    validation

--- microsite-add-edit-process.php
+++ microsite-add-edit-process.php
@@ -135,6 +135,7 @@ echo "<pre>"; print_r($_REQUEST['imageName']);
 //die;
 if(count($_REQUEST['imageName'][0]) == 0)
 	$ErrorMsg['imgTitleName'] = "Please select atleast one slider image.";
+echo count($_REQUEST['imageName'][0]);die;
     foreach($_REQUEST['imageName'] as $k=>$v){
 //echo "<br>$k".count($arrImage['imageTitle'][$k])."_".count($arrImage['imageAlt'][$k]);
         if(stristr(strtolower($_REQUEST['imageTitle'][$k]),'proptiger') || stristr(strtolower($_REQUEST['imageAlt'][$k]),'proptiger')
