1b83925f3da291bb08856839a8715720a0f08d2d
vimlesh <vimlesh@vimlesh-lp.(none)>
2015-01-06 19:39:59 +0530

    is-522

--- builderaddprocess.php
+++ builderaddprocess.php
@@ -267,6 +267,8 @@ if ($_POST['btnSave'] == "Save")
                             $imgPath = explode("images_new/",$imgurl);
                             $ImgDbFinalPath = "/".$imgPath[1];
 
+                            
+                            
                             $rt = UpdateBuilder($txtBuilderName, $legalEntity, $txtBuilderDescription, $txtBuilderUrl,$DisplayOrder,$ImgDbFinalPath,$builderid,$address,$city,$pincode,$ceo,$employee,$established,$delivered_project,$area_delivered,$ongoing_project,$website,$revenue,$debt,$contactArr,$oldbuilder, $image_id);
                             if($rt)
                             {
@@ -280,6 +282,24 @@ if ($_POST['btnSave'] == "Save")
                                 $txtBuilderUrl = createBuilderURL($txtBuilderName, $builderid);
                                 $updateQuery = 'UPDATE '.RESI_BUILDER.' set URL="'.$txtBuilderUrl.'" WHERE BUILDER_ID='.$builderid;
                                 mysql_query($updateQuery) or die(mysql_error());
+
+                                //update all project url if builder name update
+                                if($txtBuilderUrlOld != $txtBuilderUrl){
+                                    $qryPDetail = "select rp.project_id,rp.project_name,rp.project_url,l.label as localityName,c.label as cityName from RESI_PROJECT rp
+                                                   join locality l on rp.locality_id = l.locality_id
+                                                   join suburb s on l.suburb_id = s.suburb_id
+                                                   join city c on s.city_id = c.city_id where rp.builder_id = $builderid and rp.version = 'Cms'";
+                                    $resPDetail = mysql_query($qryPDetail) or die(mysql_error());
+                                    if(mysql_num_rows($resPDetail)>0){
+                                        while($data = mysql_fetch_assoc($resPDetail)){
+                                            $txtProjectURL = createProjectURL($data['cityName'], $data['localityName'], $txtBuilderName, $data['project_name'], $data['project_id']);
+                                            $updateQuery = "UPDATE ".RESI_PROJECT." set PROJECT_URL='".$txtProjectURL."' 
+                                                    where PROJECT_ID=".$data['project_id']." and version = 'Cms'";
+                                            $resUrl = mysql_query($updateQuery) or die(mysql_error());
+                                        }
+                                    }
+                                }
+                                //end code update all project url if builder name update
                                 header("Location:BuilderList.php?page=1&sort=all");
                             } else
                                 $ErrorMsg['dataInsertionError'] = "Please try again there is a problem";
@@ -377,6 +397,24 @@ if ($_POST['btnSave'] == "Save")
                         $txtBuilderUrl = createBuilderURL($txtBuilderName, $builderid);
                         $updateQuery = 'UPDATE '.RESI_BUILDER.' set URL="'.$txtBuilderUrl.'" WHERE BUILDER_ID='.$builderid;
                         mysql_query($updateQuery) or die(mysql_error());
+
+                        //update all project url if builder name update
+                            if($txtBuilderUrlOld != $txtBuilderUrl){
+                                $qryPDetail = "select rp.project_id,rp.project_name,rp.project_url,l.label as localityName,c.label as cityName from RESI_PROJECT rp
+                                               join locality l on rp.locality_id = l.locality_id
+                                               join suburb s on l.suburb_id = s.suburb_id
+                                               join city c on s.city_id = c.city_id where rp.builder_id = $builderid and rp.version = 'Cms'";
+                                $resPDetail = mysql_query($qryPDetail) or die(mysql_error());
+                                if(mysql_num_rows($resPDetail)>0){
+                                    while($data = mysql_fetch_assoc($resPDetail)){
+                                        $txtProjectURL = createProjectURL($data['cityName'], $data['localityName'], $txtBuilderName, $data['project_name'], $data['project_id']);
+                                        $updateQuery = "UPDATE ".RESI_PROJECT." set PROJECT_URL='".$txtProjectURL."' 
+                                                where PROJECT_ID=".$data['project_id']." and version = 'Cms'";
+                                        $resUrl = mysql_query($updateQuery) or die(mysql_error());
+                                    }
+                                }
+                            }
+                        //end code update all project url if builder name update
                         header("Location:BuilderList.php?page=1&sort=all");
                     }
                     else
