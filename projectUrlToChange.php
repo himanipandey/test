ba657fab95c52bb9ec835a4547347419e9a7ee4e
vimleshrajput <vimlesh.rajput@proptiger.com>
2015-01-07 15:23:01 +0530

    remove comma

--- projectUrlToChange.php
+++ projectUrlToChange.php
@@ -1,19 +1,19 @@
 <?php
 
 	error_reporting(1);
-ini_set('display_errors','1');
+    ini_set('display_errors','1');
 	include("smartyConfig.php");
 	include("appWideConfig.php");
 	include("dbConfig.php");
-	include("includes/configs/configs.php");
+	//include("includes/configs/configs.php");
 
 	AdminAuthentication();
 
-    $qryPDetail = "select rp.project_id,rp.project_name,rp.project_url,l.label as localityName,c.label as cityName,rb.builder_name from ".RESI_PROJECT." rp
+    $qryPDetail = "select rp.project_id,rp.project_name,rp.project_url,l.label as localityName,c.label as cityName,rb.builder_name from resi_project rp
                     join resi_builder rb on rp.builder_id = rb.builder_id
                    join locality l on rp.locality_id = l.locality_id
                    join suburb s on l.suburb_id = s.suburb_id
-                   join city c on s.city_id = c.city_id where rp.version = 'Cms'";
+                   join city c on s.city_id = c.city_id where rp.version = 'Cms' limit 10";
     $resPDetail = mysql_query($qryPDetail) or die(mysql_error());
 
 echo "Following project url not matched";
@@ -21,9 +21,9 @@ echo "Following project url not matched";
         while($data = mysql_fetch_assoc($resPDetail)){
             $txtProjectURL = createProjectURL($data['cityName'], $data['localityName'], $data['builder_name'], $data['project_name'], $data['project_id']);
             
-            if($data['localityName'] != $txtProjectURL){
+            //if($data['localityName'] != $txtProjectURL){
                 echo "Existing URL: ".$data['project_url'] ."<====>According to current format: ". $txtProjectURL."<br>";
-            }
+            //}
            // $updateQuery = "UPDATE ".RESI_PROJECT." set PROJECT_URL='".$txtProjectURL."' 
                 //    where PROJECT_ID=".$data['project_id']." and version = 'Cms'";
             //$resUrl = mysql_query($updateQuery) or die(mysql_error());
